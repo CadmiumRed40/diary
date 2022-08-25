d4db76a2411c418a539efa97f3099a9d91640797
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2022-08-24 23:59:37 -0400

    user auth

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 //loaded config
 dotenv.config({ path: './config/config.env' })
+
+//passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -21,11 +26,25 @@ if (process.env.NODE_ENV === 'developement'){
 app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'})) //use exphbs.engine to avoid errors. ".engine() calls the engine function from the "express-handlebars" package"
 app.set('view engine', '.hbs')
 
+// Sessions
+app.use(session({
+    secret: 'erman walt',
+    resave: false,
+    saveUninitialized: false,
+    })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 //Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 //routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('.routes/auth'))
 
 const PORT = process.env.PORT || 3000
 

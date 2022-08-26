d8da4187641f8abbb5008978abc2b0ffa27a1de3
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2022-08-26 13:42:59 -0400

    route error fixed

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 //routes
 app.use('/', require('./routes/index'))
-app.use('/auth', require('.routes/auth'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3000
 

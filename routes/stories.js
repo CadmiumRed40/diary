76a190a7dfaea24ab11960dacf6e9951f392bc1e
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2022-09-27 18:51:48 -0400

    final functions done. Must debug

--- routes/stories.js
+++ routes/stories.js
@@ -44,6 +44,27 @@ router.get('/', ensureAuth , async (req,res)=> {
     }
 })
 
+// @desc Show single story 
+// @route GET /stories/:id
+
+router.get('/:id', ensureAuth, async (req, res) => {
+    try{
+        let story = await Story.findById(req.params.id)
+        .populate('user')
+        .lean()
+
+    if (!story) {
+        return res.render('error/404')
+    }
+
+    res.render('stories/show', {
+        story
+    })
+    }catch (err){
+        res.render('error/404')
+    }
+})
+
 //@desc Show edit page 
 //@desc GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -112,5 +133,24 @@ router.delete('/:id', ensureAuth , async (req,res)=> {
     }
 })
 
+// @desc User Stories
+// @route GET /stories/user/:userid
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public'
+        })
+        .populate('user')
+        .lean()
 
+        res.render('stories/index', {
+            stories
+        })
+    } catch(err){
+        console.error(err)
+        res.render('error/500')
+
+    }
+})
 module.exports = router
